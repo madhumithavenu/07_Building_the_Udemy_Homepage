d51545e8a5ce73a595f4d75e05701a8d7118d9a7
Madhumitha <madhumithavenu03@gmail.com>
2023-07-24 16:09:05 +0530

    Setup gulp complete

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 
-//const sass = require('gulp-sass')(require('sass'));
 
 function css() {
     return gulp
