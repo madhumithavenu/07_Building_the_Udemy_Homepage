d6a4cac6eb484157e914009c01129764db4881a9
Madhumitha <madhumithavenu03@gmail.com>
2023-07-24 16:31:12 +0530

    Creating a Gulp Overflow

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,17 @@ const autoprefixer = require('gulp-autoprefixer');
 function css() {
     return gulp
         .src('scss/app.scss')
-        .pipe(autoprefixer())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions']
+        }))
         .pipe(sass({ outputStyle: 'expanded' }))
         .pipe(gulp.dest('css'));
 }
 
+function watchFiles(){
+    gulp.watch('scss/*.scss', css);
+    gulp.watch('index.html')
+}
+
 gulp.task('css', css);
+gulp.task('watch', gulp.parallel(watchFiles));
